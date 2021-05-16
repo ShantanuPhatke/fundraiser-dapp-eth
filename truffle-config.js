d5f6f9a743b010ba16f98a7de5e37b6ac444455b
shantanuphatke <sphatkes@gmail.com>
2021-05-17 01:04:11 +0530

    deployed to ropsten net

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 const path = require("path")
+const fs = require("fs")
+
+const HDWalletProvider = require("@truffle/hdwallet-provider")
+const secrets = JSON.parse(fs.readFileSync(".secrets").toString().trim())
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +11,18 @@ module.exports = {
   networks: {
     develop: {
       port: 8545,
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          secrets.seed,
+          `https://ropsten.infura.io/v3/${secrets.infuraProjectId}`
+        ),
+      network_id: 3,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
   },
   compilers: {
