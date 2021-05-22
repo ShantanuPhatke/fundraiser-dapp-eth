f9ff9c1a987bd6a1536791f5482e036104c11ff2
shantanuphatke <sphatkes@gmail.com>
2021-05-23 00:35:15 +0530

    added form validation to create-fundraiser form

--- client/src/pages/CreateFundraiser/CreateFundraiser.js
+++ client/src/pages/CreateFundraiser/CreateFundraiser.js
@@ -1,17 +1,55 @@
 import React, { useContext } from "react"
 import { useForm } from "react-hook-form"
 import { Context } from "../../Context"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+const tomorrow = new Date(Date.now() + 86400000)
+tomorrow.setHours(0, 0, 0, 0)
+
+const formatDate = (date) => {
+  let dd = date.getDate()
+  let mm = date.getMonth() + 1 //January is 0 so need to add 1 to make it 1!
+  let yyyy = date.getFullYear()
+  if (dd < 10) {
+    dd = "0" + dd
+  }
+  if (mm < 10) {
+    mm = "0" + mm
+  }
+
+  return yyyy + "-" + mm + "-" + dd
+}
 
 function CreateFundraiser() {
   const { web3, accounts, contract, updateFundraisers } = useContext(Context)
-
   const toWei = (amount) => web3.utils.toWei(amount, "ether")
+
+  const schema = yup.object().shape({
+    hostName: yup.string().required().min(3),
+    title: yup.string().required().min(3),
+    goalAmount: yup.number().required(),
+    minDonation: yup.number().required(),
+    expiryDate: yup
+      .date()
+      .required()
+      .min(tomorrow, "Expiry Date of the fundraiser should be a later date"),
+    recipientAddress: yup
+      .string()
+      .required()
+      .test("isAddress", "Enter a valid ethereum address", function (value) {
+        return web3.utils.isAddress(value)
+      }),
+    description: yup.string().required().min(200),
+  })
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
 
   const dateToBigInt = (date) => new Date(date).getTime() / 1000
 
@@ -60,53 +98,56 @@ function CreateFundraiser() {
                 name="hostName"
                 id="hostName"
                 placeholder="hostName"
-                {...register("hostName", { required: true })}
+                {...register("hostName")}
               />
-              {errors.hostName && <span>This field is required</span>}
+              {errors.hostName && <span>{errors.hostName.message}</span>}
 
               <input
                 type="text"
                 name="title"
                 id="title"
                 placeholder="title"
-                {...register("title", { required: true })}
+                {...register("title")}
               />
-              {errors.title && <span>This field is required</span>}
+              {errors.title && <span>{errors.title.message}</span>}
 
               <input
                 type="number"
                 name="goalAmount"
                 id="goalAmount"
                 placeholder="goalAmount (ETH)"
-                {...register("goalAmount", { required: true })}
+                {...register("goalAmount")}
               />
-              {errors.goalAmount && <span>This field is required</span>}
+              {errors.goalAmount && <span>{errors.goalAmount.message}</span>}
 
               <input
                 type="number"
                 name="minDonation"
                 id="minDonation"
                 placeholder="minDonation (ETH)"
-                {...register("minDonation", { required: true })}
+                {...register("minDonation")}
               />
-              {errors.minDonation && <span>This field is required</span>}
+              {errors.minDonation && <span>{errors.minDonation.message}</span>}
 
               <input
                 type="date"
                 name="expiryDate"
                 id="expiryDate"
-                {...register("expiryDate", { required: true })}
+                min={formatDate(tomorrow)}
+                {...register("expiryDate")}
               />
-              {errors.expiryDate && <span>This field is required</span>}
+              {errors.expiryDate && <span>{errors.expiryDate.message}</span>}
 
               <input
                 type="text"
                 name="recipientAddress"
                 id="recipientAddress"
                 placeholder="recipientAddress"
-                {...register("recipientAddress", { required: true })}
+                {...register("recipientAddress")}
               />
-              {errors.recipientAddress && <span>This field is required</span>}
+              {errors.recipientAddress && (
+                <span>{errors.recipientAddress.message}</span>
+              )}
 
               <textarea
                 name="description"
@@ -114,9 +155,9 @@ function CreateFundraiser() {
                 cols="30"
                 rows="10"
                 placeholder="description"
-                {...register("description", { required: true })}
+                {...register("description")}
               ></textarea>
-              {errors.description && <span>This field is required</span>}
+              {errors.description && <span>{errors.description.message}</span>}
 
               <input type="submit" value="submit" />
             </form>
